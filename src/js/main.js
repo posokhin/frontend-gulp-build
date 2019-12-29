6c41faa09d2a232170bdf7c52852886d4feb8612
unknown <C:\Users\Anton\AppData\Roaming\The Bat!>
2019-12-29 16:00:17 +0300

    reference animate

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,15 @@
 var $ = require('jquery');
 
 $(document).ready(function(){
-    console.log(1);
+
+    /* reference animate */
+    $('a[href^="#"]').on('click', function(e){
+        e.preventDefault();
+        var target = $(this.getAttribute('href'));
+        $('html,body').animate({
+            scrollTop: target.offset().top - 100
+        }, 1000);
+    });
+    /* reference animate end */
+
 });
